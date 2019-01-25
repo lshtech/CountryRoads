ed9408092d8c28b0b364dc9e17b10a78fdc695a2
lshtech <elbebeta@gmail.com>
2019-01-24 20:01:02 -0600

    checkin

---
+++ CountryRoads.Mobile/CountryRoads.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CountryRoads.Mobile.Models;
+
+namespace CountryRoads.Mobile.ViewModels
+{
+  public class ItemDetailViewModel : BaseViewModel
+  {
+    public Item Item { get; set; }
+    public ItemDetailViewModel(Item item = null)
+    {
+      Title = item?.Text;
+      Item = item;
+    }
+  }
+}
